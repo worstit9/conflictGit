4cce28da7ce89b3043babe1aeb662cb0056208f1
worstit9 <56362276+worstit9@users.noreply.github.com>
2021-06-15 22:22:44 +0800

    remove funs

--- hello.py
+++ hello.py
@@ -1,14 +1,6 @@
 def fun1():
     print("hello first commit")
     
-def fun2():
-    print("hello fun2 new branch")
 
-def fun3():
-    return 1
-
-def fun4():
-	print("something going to commit (user1)")
-	print("some another message")
 
 fun1()
